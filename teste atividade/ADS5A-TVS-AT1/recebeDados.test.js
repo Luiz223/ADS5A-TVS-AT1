116ca623b23de03befc23b87419222dbf396780d
Luiz223 <119684132+Luiz223@users.noreply.github.com>
2024-04-11 17:26:20 -0300

    Add files via upload

---
+++ teste atividade/ADS5A-TVS-AT1/recebeDados.test.js
@@ -0,0 +1,12 @@
+
+const dadosEstudante = require('./dadosEstudante');
+
+describe('Teste da função recebeDados', () => {
+  test('Verifica se a função processa corretamente o nome "Luiz Henrique" e o RA "191192"', () => {
+    const nome = "Luiz Henrique";
+    const RA = "191192";
+    const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
+
+    expect(dadosEstudante(nome, RA)).toBe(esperado);
+  });
+});
